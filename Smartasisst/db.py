0f5442f5c22a104b09c8bc1f7aff1a36dbbc67bb
leandro oviedo <121698822+DvCorsair@users.noreply.github.com>
2026-08-16 16:40:31 -0300

    Add files via upload

--- Smartasisst/db.py
+++ Smartasisst/db.py
@@ -72,7 +72,7 @@ def guardar_reclamo(
 def mostrar_historial():
 
     conexion = sqlite3.connect(
-        "datos/reclamos.db"
+        "C:/Users/leand/OneDrive/Desktop/Smartasisst/datos/reclamos.db"
     )
 
     cursor = conexion.cursor()
